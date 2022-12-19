4dac961474040b5658c091899250a21f25f527e6
caseyg27 <119962405+caseyg27@users.noreply.github.com>
2022-12-19 10:29:38 -0500

    Initial commit

---
+++ Animal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+// Base Class
+
+public class Animal
+{
+    public string name;
+    public int age;
+    public string color;
+    public string gender;
+    public float weight;
+
+    public Animal()
+    {
+    }
+
+    public virtual void Bark()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+//Derived Class
+
+public class Dog : Animal
+{
+    public string breed;
+    public string ownerName;
+    public bool rabbiesShot;
+    public Dog()
+    {
+
+    }
+}
+
+
+public class Cat : Animal
+{
+    public string breed;
+    public bool houseCat;
+    public bool hair;
+    public Cat()
+    {
+
+    }
+    public override void Bark()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+public class Fish : Animal
+{
+    public string WaterType;
+    public bool zoo;
+    public Fish()
+    {
+
+    }
+
+    public override void Bark()
+    {
+        Console.WriteLine("Glumb Glumb");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Program Name:Pet Creater
+ * Programmer: Casey Giancaterino
+ * Date: 12/19/2022
+ *
+ * Description: This program allows you to create a pet and create a unique name,age,gender,color weight and owner. 
+ * The program also allows you to answer questions about the animal to make it itslef.
+ */
+
+Animal Wednesday = new Animal();
+
+
+Wednesday.name = "Wednesday";
+Wednesday.age = 15;
+Wednesday.gender = "Female";
+Wednesday.color = "Black";
+Wednesday.weight = 82;
+
+
+Dog Bill = new Dog();
+
+Bill.name = "Bill";
+Bill.age = 9;
+Bill.gender = "Male";
+Bill.color = "White";
+Bill.weight = 35;
+Bill.breed = "Labradoodle";
+Bill.ownerName = "Bill Nye The Science Guy";
+Bill.rabbiesShot = true;
+Bill.Bark();
+
+
+Cat Waltuh = new Cat();
+
+Waltuh.name = "Waltuh";
+Waltuh.age = 17;
+Waltuh.gender = "Male";
+Waltuh.color = "Black and Blue";
+Waltuh.weight = 46;
+Waltuh.breed = "Black Cat";
+Waltuh.houseCat = true;
+Waltuh.hair = true;
+Waltuh.Bark();
+
+
+Fish deshaun = new Fish();
+
+deshaun.name = "deshaun";
+deshaun.age = 2;
+deshaun.gender = "Male";
+deshaun.color = "Orange and Black";
+deshaun.weight = 2;
+deshaun.WaterType = "Salt";
+deshaun.zoo = true;
+deshaun.Bark();
